b020b3571beb3718d147928eaf7004c94addd555
brenohq <brenosc2@hotmail.com>
2024-01-11 11:08:18 -0300

    singly liked list impl

--- src/__tests__/ListTest.ts
+++ src/__tests__/ListTest.ts
@@ -3,7 +3,11 @@ export function test_list(list: List<number>): void {
     list.append(7);
     list.append(9);
 
+    expect(list.get(0)).toEqual(5);
+    expect(list.get(1)).toEqual(7);
     expect(list.get(2)).toEqual(9);
+    expect(list.length).toEqual(3);
+
     expect(list.removeAt(1)).toEqual(7);
     expect(list.length).toEqual(2);
 
@@ -22,5 +26,7 @@ export function test_list(list: List<number>): void {
     expect(list.get(0)).toEqual(9);
     expect(list.remove(9)).toEqual(9);
     expect(list.length).toEqual(2);
+    expect(list.remove(5)).toEqual(5);
+    expect(list.length).toEqual(1);
     expect(list.get(0)).toEqual(7);
 }
--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,112 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+}
+
 export default class SinglyLinkedList<T> {
     public length: number;
 
-    
+    private start?: Node<T>;
+    private end?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.start = this.end = undefined;
     }
 
     prepend(item: T): void {
+        const old_start = this.start;
 
-}
+        this.start = { value: item } as Node<T>;
+        this.start.next = old_start;
+
+        this.length++;
+    }
+
     insertAt(item: T, idx: number): void {
 
-}
+    }
+
     append(item: T): void {
+        const node = { value: item } as Node<T>;
 
-}
+        this.length++;
+
+        if (!this.start) {
+            this.start = this.end = node;
+            this.start.next = node;
+            return;
+        }
+
+        if (this.end) {
+            this.end.next = node;
+            this.end = node;
+            return;
+        }
+    }
+
     remove(item: T): T | undefined {
+        let current = this.start;
 
-}
+        if (this.start?.value === item) {
+            const to_remove = this.start;
+            this.start = this.start.next
+            this.length--;
+            return to_remove.value;
+        }
+
+        for (let i = 0; i < this.length; i++) {
+            if (current?.next?.value === item) {
+                const to_remove = current.next;
+                current = current.next?.next
+                this.length--;
+                return to_remove?.value;
+            } else if (!current?.next) {
+                return undefined
+            }
+            current = current.next;
+        }
+
+        return undefined;
+    }
+
     get(idx: number): T | undefined {
+        let current = this.start;
 
-}
+        for (let i = 0; i < this.length; i++) {
+            if (i === idx) {
+                return current?.value;
+            } else if (!current?.next) {
+                return undefined
+            }
+            current = current.next;
+        }
+
+        return undefined;
+    }
+
     removeAt(idx: number): T | undefined {
+        let current = this.start;
 
-}
+        if (idx > this.length) return undefined;
+
+        if (idx === 0) {
+            const to_remove = this.start;
+            this.start = this.start?.next;
+            this.length--;
+            return to_remove?.value;
+        }
+
+        for (let i = 0; i < this.length; i++) {
+            if (idx - 1 === i && current) {
+                const to_remove = current?.next;
+                current.next = current?.next?.next
+                this.length--;
+                return to_remove?.value;
+            }
+            current = current?.next;
+        }
+
+        return undefined;
+    }
 }
